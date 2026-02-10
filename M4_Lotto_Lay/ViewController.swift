9b366e085c9e07a27c2d199b3049dbef53f42d69
Dustin <redhotblood1010@gmail.com>
2026-02-10 11:41:17 -0500

    Initial Commit

---
+++ M4_Lotto_Lay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M4_Lotto_Lay
+//
+//  Created by Heather Bishop on 2/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
