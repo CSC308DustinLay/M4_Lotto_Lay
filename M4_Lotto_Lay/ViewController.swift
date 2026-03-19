7da39560ae446803e7de690f093ba445d24ddf7b
Dustin <redhotblood1010@gmail.com>
2026-03-19 12:39:08 -0400

    sure

--- M4_Lotto_Lay/ViewController.swift
+++ M4_Lotto_Lay/ViewController.swift
@@ -17,11 +17,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var label7: UILabel!
     @IBOutlet weak var label8: UILabel!
     
+    
+    @IBOutlet var labels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         
         let labels = [label1!, label2!, label3!, label4!, label5!, label6!, label8!]
         
@@ -35,38 +38,58 @@ class ViewController: UIViewController {
                 nums.append(rand)
             }
         }
-        
-        let sortedNums = nums.sorted()
+        nums.sort()
+        //let sortedNums = nums.sorted()
         
         for (index,label) in labels.enumerated()
         {
-            label.layer.cornerRadius = label.bounds.width / 2
             label.clipsToBounds = true
             
-            label.text = "\(sortedNums[index])"
+            label.text = "\(nums[index])"
             
-            switch sortedNums[index]
-            {
-            case 1...10:
-                label.backgroundColor = UIColor.red
-            case 11...20:
-                label.backgroundColor = UIColor.green
-            case 21...30:
-                label.backgroundColor = UIColor.blue
-            case 31...40:
-                label.backgroundColor = UIColor.yellow
-            case 41...45:
-                label.backgroundColor = UIColor.purple
-            default:
-                break
-            }
+            label.backgroundColor = getColors(from: nums[index]).backgroundColor
+            label.textColor = getColors(from: nums[index]).textColor
         }
         label7.layer.cornerRadius = label7.bounds.width / 2
         label7.clipsToBounds = true
-        label7.backgroundColor = UIColor.purple
-        label7.textColor = .white
+        let colors = getColors(from: nil)
+        label7.backgroundColor = colors.backgroundColor
+        label7.textColor = colors.textColor
         
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator)
+    {
+        coordinator.animate { _ in
+            for label in self.labels
+            {
+                label.layer.cornerRadius = label.bounds.width / 2
+                label.clipsToBounds = true
+            }
+        }
 
+
+    }
 }
 
+func getColors(from number: Int?) -> (backgroundColor: UIColor, textColor: UIColor)
+{
+    guard let number else{
+        return (UIColor.purple, UIColor.white)
+    }
+    switch number
+    {
+    case 1...10:
+        return(UIColor.red, UIColor.white)
+    case 11...20:
+        return(UIColor.green, UIColor.black)
+    case 21...30:
+        return(UIColor.blue, UIColor.white)
+    case 31...40:
+        return(UIColor.yellow, UIColor.black)
+    case 41...45:
+        return(UIColor.gray, UIColor.white)
+    default:
+        return (UIColor.purple, UIColor.white)
+    }
+}
